5408c6b58e8cab90fe76ad490db536d46109406f
Francisco García <franciscojavgm@gmail.com>
2022-01-08 14:49:51 -0500

    update/inicio login y registro

---
+++ app/src/main/java/com/hexagonal/vaquita/ActividadLogin.kt
@@ -0,0 +1,42 @@
+package com.hexagonal.vaquita
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ActividadLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //boton login
+        var buttonEntrar = findViewById<Button>(R.id.botonLogin)
+        buttonEntrar.setOnClickListener{
+            val intencion = Intent(this,ActividadInicio::class.java)
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage(R.string.loremIpsum)
+                .setPositiveButton(R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        startActivity(intencion)
+                    })
+                .setNegativeButton(R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        return@OnClickListener
+                    })
+            builder.create()
+            builder.show()
+        }
+
+        // boton registro
+        var botonRegistro = findViewById<Button>(R.id.botonRegistro)
+        botonRegistro.setOnClickListener{
+            //Toast.makeText(this, "Registro exitoso", Toast.LENGTH_LONG).show()
+            val intencion = Intent(this,ActividadRegistro::class.java)
+            //intencion.putExtra("Extra",extraerNombreApellido(textViewMail.text.toString()))
+            startActivity(intencion)
+        }
+    }
+}
---
+++ app/src/main/java/com/hexagonal/vaquita/ActividadRegistro.kt
@@ -0,0 +1,38 @@
+package com.hexagonal.vaquita
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+
+class ActividadRegistro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_registro)
+
+        // boton registro
+        var botonRegistro = findViewById<Button>(R.id.botonRegistro)
+        botonRegistro.setOnClickListener{
+            //builder del diálogo
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage(R.string.exitoRegistro)
+                .setPositiveButton(R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        //envío a inicio
+                        val intencion = Intent(this,ActividadInicio::class.java)
+                        startActivity(intencion)
+                    })/*
+                .setNegativeButton(R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })*/
+            builder.create()
+            builder.show()
+        }
+    }
+}
+
