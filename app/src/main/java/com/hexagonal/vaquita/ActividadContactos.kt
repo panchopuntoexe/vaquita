3eeb91ad1348ca9e91f33145ff1ccd7fe2e2f322
Francisco García <franciscojavgm@gmail.com>
2022-01-08 23:06:58 -0500

    update/Pagos a realizar

--- app/src/main/java/com/hexagonal/vaquita/ActividadContactos.kt
+++ app/src/main/java/com/hexagonal/vaquita/ActividadContactos.kt
@@ -5,7 +5,6 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
 class ActividadContactos : AppCompatActivity() {
@@ -13,7 +12,7 @@ class ActividadContactos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_contactos)
         // boton guardar Contactos
-        var botonSalvar = findViewById<Button>(R.id.botonSalvar)
+        var botonSalvar = findViewById<Button>(R.id.botonPagar)
         botonSalvar.setOnClickListener{
             val intencion = Intent(this,ActividadInicio::class.java)
             val builder = AlertDialog.Builder(this)
