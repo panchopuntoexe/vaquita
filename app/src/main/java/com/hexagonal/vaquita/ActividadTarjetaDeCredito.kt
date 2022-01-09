82ea8c85018be7655331e2492260d8ae1b76a36e
Francisco García <franciscojavgm@gmail.com>
2022-01-09 10:01:06 -0500

    update/Pago con tarjeta

--- app/src/main/java/com/hexagonal/vaquita/ActividadTarjetaDeCredito.kt
+++ app/src/main/java/com/hexagonal/vaquita/ActividadTarjetaDeCredito.kt
@@ -1,11 +1,20 @@
 package com.hexagonal.vaquita
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ActividadTarjetaDeCredito : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_tarjeta_de_credito)
+
+        // boton  pago
+        var botonTarjeta = findViewById<Button>(R.id.botonPagarTarjeta)
+        botonTarjeta.setOnClickListener{
+            val intencion = Intent(this,ActividadInicio::class.java)
+            startActivity(intencion)
+        }
     }
 }
