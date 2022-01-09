af99519ef3ec0e346be5abc0f9e9676fe537e8ba
Francisco García <franciscojavgm@gmail.com>
2022-01-08 23:20:14 -0500

    update/Diseño pagos

--- app/src/main/java/com/hexagonal/vaquita/ActividadCompra.kt
+++ app/src/main/java/com/hexagonal/vaquita/ActividadCompra.kt
@@ -1,12 +1,20 @@
 package com.hexagonal.vaquita
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ActividadCompra : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_compra)
 
+        // boton  método de pago
+        var botonMetodoDePago = findViewById<Button>(R.id.botonPagar)
+        botonMetodoDePago.setOnClickListener{
+            val intencion = Intent(this,ActividadMetodoDePago::class.java)
+            startActivity(intencion)
+        }
     }
 }
