f110074868a08c30e371a90bce51d89f0ad81ba8
Peterca <pierre.dolce@epn.edu.ec>
2022-01-09 23:27:31 -0500

    update/ agregar panatalla de ingresar gastos

---
+++ app/src/main/java/com/hexagonal/vaquita/ActivityAgregarGasto.kt
@@ -0,0 +1,11 @@
+package com.hexagonal.vaquita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityAgregarGasto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_gasto)
+    }
+}
