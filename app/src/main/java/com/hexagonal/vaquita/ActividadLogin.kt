790b28a4bc8ce8456cd6fc5445b05b38cce6d395
Juan Pablo Tovar <juan.tovar@epn.edu.ec>
2022-02-06 01:06:16 -0500

    Update / UI subir archivos

--- app/src/main/java/com/hexagonal/vaquita/ActividadLogin.kt
+++ app/src/main/java/com/hexagonal/vaquita/ActividadLogin.kt
@@ -3,14 +3,21 @@ package com.hexagonal.vaquita
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class ActividadLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        leerDatos()
 
         //boton login
         var buttonEntrar = findViewById<Button>(R.id.botonLogin)
@@ -39,7 +46,38 @@ class ActividadLogin : AppCompatActivity() {
             startActivity(intencion)
         }
 
+    }
 
+    fun probarBase (){
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "Nombre" to "Ada",
+            "Apeyido" to "Lovelace",
+        )
+        val TAG = ""
+        db.collection("Usuarios")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+    fun leerDatos(){
+        val db = Firebase.firestore
+        val TAG = "Usuarios: "
 
+        db.collection("Usuarios").whereArrayContains("Wallets","Sh2JdyEeLvu1LORENjNm")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document}")
+                }
+//                Log.d(TAG, "${result}")
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 }
