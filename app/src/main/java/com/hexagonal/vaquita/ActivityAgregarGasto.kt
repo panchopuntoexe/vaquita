511d9b270e2ced4530974904ed3ded39e8d5171f
Francisco García <franciscojavgm@gmail.com>
2022-02-17 16:17:38 -0500

    update / actividad compra

--- app/src/main/java/com/hexagonal/vaquita/ActivityAgregarGasto.kt
+++ app/src/main/java/com/hexagonal/vaquita/ActivityAgregarGasto.kt
@@ -1,11 +1,20 @@
 package com.hexagonal.vaquita
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ActivityAgregarGasto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_gasto)
+
+        // boton  agregar
+        var botonMetodoDePago = findViewById<Button>(R.id.button4)
+        botonMetodoDePago.setOnClickListener {
+            val intencion = Intent(this, ActividadInicio::class.java)
+            startActivity(intencion)
+        }
     }
 }
