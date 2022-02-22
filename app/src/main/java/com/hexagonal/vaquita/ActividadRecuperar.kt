fc3ffc354633f2f209643f1a899c5aeb5cec8cec
David <dave_martins@hotmail.com>
2022-02-21 19:51:30 -0500

    Update/ Recuperar Contraseña y Guardar Shared Preferences

---
+++ app/src/main/java/com/hexagonal/vaquita/ActividadRecuperar.kt
@@ -0,0 +1,77 @@
+package com.hexagonal.vaquita
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+
+
+class ActividadRecuperar : AppCompatActivity() {
+
+    private lateinit var email: EditText
+    private lateinit var recuperar: Button
+    private lateinit var progress: ProgressDialog
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_recuperar)
+
+        email = findViewById(R.id.textEmailRecuperar)
+        recuperar = findViewById(R.id.botonRecuperar)
+
+        auth = FirebaseAuth.getInstance();
+        progress = ProgressDialog(this);
+
+        getRecuperar();
+    }
+
+    private fun getRecuperar() {
+        recuperar.setOnClickListener() {
+            if (email.text.isNotEmpty()) {
+                progress.setMessage("Espere un momento...");
+                progress.setCanceledOnTouchOutside(false);
+                progress.show();
+                getEnviarCorreo();
+            } else {
+                showAlert()
+            }
+        }
+    }
+
+    private fun getEnviarCorreo() {
+        auth.setLanguageCode("es");
+        auth.sendPasswordResetEmail(email.text.toString().trim()).addOnCompleteListener() { task ->
+            if (task.isSuccessful()) {
+                showMessage()
+            }else{
+                showAlert()
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("El correo no se pudo enviar")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showMessage() {
+        Toast.makeText(
+            this,
+            "Por favor revise su correo para restaurar la contraseña",
+            Toast.LENGTH_LONG
+        ).show()
+        val loginIntent = Intent(this, ActividadLogin::class.java)
+        startActivity(loginIntent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/hexagonal/vaquita/datos/FileExternalManager.kt
@@ -0,0 +1,65 @@
+package com.hexagonal.vaquita.datos
+
+import android.app.Activity
+import android.os.Environment
+import com.hexagonal.vaquita.SHAREDINFO_FILENAME
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+class FileExternalManager(val actividad: Activity): FileHandler{
+
+    // Comprobando que el almacenamiento externo sea escribible
+    fun isExternalStorageWritable(): Boolean {
+        return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+    }
+
+    // Guardando la Información
+    override fun SaveInformation(datosAGrabar: Pair<String, String>) {
+        if (isExternalStorageWritable()) {
+            FileOutputStream(
+                File(
+                    actividad.getExternalFilesDir(null),
+                    SHAREDINFO_FILENAME
+                )
+            ).bufferedWriter().use { outputStream ->
+                outputStream.write(datosAGrabar.first)
+                outputStream.write(System.lineSeparator())
+                outputStream.write(datosAGrabar.second)
+            }
+        }
+    }
+
+    // Comprobación si el almacenamiento externo es leíble
+    fun isExternalStorageReadable(): Boolean {
+        return Environment.getExternalStorageState() in
+                setOf(Environment.MEDIA_MOUNTED, Environment.MEDIA_MOUNTED_READ_ONLY)
+    }
+
+    // Lectura de la Información
+    override fun ReadInformation(): Pair<String, String> {
+        var email=""
+        var clave=""
+        try {
+            if (isExternalStorageReadable()) {
+                FileInputStream(
+                    File(
+                        actividad.getExternalFilesDir(null),
+                        SHAREDINFO_FILENAME
+                    )
+                ).bufferedReader().use {
+                    val datoLeido = it.readText()
+                    val textArray = datoLeido.split(System.lineSeparator())
+                    email = textArray[0]
+                    clave = textArray[1]
+                }
+            }
+            return (email to clave)
+        }catch (e:Exception){
+            return "" to ""
+        }
+
+
+    }
+
+}
