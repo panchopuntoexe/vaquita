29fba4fbeaed4599356dc5dc5868ced40308f1f8
Juan Pablo Tovar <juan.tovar@epn.edu.ec>
2022-02-27 21:44:02 -0500

    Update / Recycler view contactos

--- app/src/main/java/com/hexagonal/vaquita/ActividadContactos.kt
+++ app/src/main/java/com/hexagonal/vaquita/ActividadContactos.kt
@@ -3,16 +3,51 @@ package com.hexagonal.vaquita
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.hexagonal.vaquita.adapters.UserAdapter
+import com.hexagonal.vaquita.databinding.ActivityActividadContactosBinding
+import com.hexagonal.vaquita.entidades.Usuario
+
 
 class ActividadContactos : AppCompatActivity() {
+
+    private var usuarios=ArrayList<Usuario>()
+
+
+    private lateinit var binding: ActivityActividadContactosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_contactos)
         // boton guardar Contactos
         var botonSalvar = findViewById<Button>(R.id.botonPagar)
+        getUsuarios()
+
+//        lateinit var userAdapter: UserAdapter
+//
+//        lateinit var layoutManager: RecyclerView.LayoutManager
+//
+//
+//        binding = ActivityActividadContactosBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        userAdapter = UserAdapter(this, usuarios, R.layout.contactos_layout)
+//
+//        layoutManager = GridLayoutManager(this, 1)
+//
+//        binding.recyclerView.setHasFixedSize(true)
+//        binding.recyclerView.layoutManager = layoutManager
+//        binding.recyclerView.adapter = userAdapter
+//        binding.recyclerView.setAdapter(userAdapter);
+//
+//
+//
         botonSalvar.setOnClickListener{
             val intencion = Intent(this,ActividadHome::class.java)
             val builder = AlertDialog.Builder(this)
@@ -23,6 +58,28 @@ class ActividadContactos : AppCompatActivity() {
                     })
             builder.create()
             builder.show()
+        }
+    }
+
+    fun getUsuarios(){
+        var db = Firebase.firestore
+        db.collection("Usuarios").get().addOnSuccessListener { result ->
+            for (document in result) {
+                usuarios.add(document.toObject(Usuario::class.java))
+            }
+            lateinit var userAdapter: UserAdapter
+
+            lateinit var layoutManager: RecyclerView.LayoutManager
+
+
+            binding = ActivityActividadContactosBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+
+            binding.recyclerView.layoutManager = LinearLayoutManager(this)
+            binding.recyclerView.adapter = UserAdapter(this, usuarios, "",R.layout.contactos_layout)
+            binding.recyclerView.setHasFixedSize(true)
+
         }
     }
 }
